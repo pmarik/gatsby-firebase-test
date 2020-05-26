c3fdca31b38bc93ed55b71f958b21eafcc1922ad
pmarik <pmarik@uwalumni.com>
2020-05-26 18:18:05 -0500

    add instead of set

--- src/components/Dashboard/Projects/ProjectsList.jsx
+++ src/components/Dashboard/Projects/ProjectsList.jsx
@@ -23,7 +23,7 @@ class ProjectsList extends Component {
       const projectName = this.state.projectName;
       const projectType = this.state.projectType;
 
-      this.props.firebase.db.collection('projects').set({
+      this.props.firebase.db.collection('projects').add({
           projectName,
           projectType,
       })
