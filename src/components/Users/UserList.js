01df44d2385ea4f3ca5abe398df8793ddf93bafe
pmarik <pmarik@uwalumni.com>
2020-05-26 16:09:31 -0500

    uselist back to class

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,122 +1,24 @@
-// import React, { Component } from 'react';
-
-// import { withFirebase } from '../Firebase';
-
-// class UserList extends Component {
-//   _initFirebase = false;
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       loading: false,
-//       users: [],
-//     };
-//   }
-
-//   firebaseInit = () => {
-//     if (this.props.firebase && !this._initFirebase) {
-//       this._initFirebase = true;
-
-//       this.setState({ loading: true });
-
-//       // this.props.firebase.users().on('value', snapshot => {
-//       //   const usersObject = snapshot.data();
-
-//       //   const usersList = Object.keys(usersObject).map(key => ({
-//       //     ...usersObject[key],
-//       //     uid: key,
-//       //   }));
-
-//       //   this.setState({
-//       //     users: usersList,
-//       //     loading: false,
-//       //   });
-//       // });
-
-//       // TODO updated to firebase calls
-//       this.props.firebase.users().get()
-//         .then( snapshot => {
-//           console.log('snapshot in userslist: ', snapshot);
-//           // const usersObject = snapshot.data();
-//           // console.log('firebase usersList users snapshot.data(): ', usersObject)
-
-//           const usersList = snapshot.docs.map(doc => doc.data());
-
-//           // const usersList = Object.keys(usersObject).map(key => ({
-//           //   ...usersObject[key],
-//           //   uid: key,
-//           // }));
-
-//           this.setState({
-//             users: usersList,
-//             loading: false,
-//           })
-//         })
-//     }
-//   };
-
-//   componentDidMount() {
-//     this.firebaseInit();
-//   }
-
-//   componentDidUpdate() {
-//     this.firebaseInit();
-//   }
-
-//   // TODO firbase equivalent for unmounting
-//   // componentWillUnmount() {
-//   //   this.props.firebase.users().off();
-//   // }
-
-//   render() {
-//     const { users, loading } = this.state;
-
-//     return (
-//       <div>
-//         <h2>Users</h2>
-//         {loading && <div>Loading ...</div>}
-
-//         <ul>
-//           {users.map(user => (
-//             <li key={user.uid}>
-//               <span>
-//                 <strong>ID:</strong> {user.uid}
-//               </span>
-//               <span>
-//                 <strong>E-Mail:</strong> {user.email}
-//               </span>
-//               <span>
-//                 <strong>Username:</strong> {user.username}
-//               </span>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 
 import { withFirebase } from '../Firebase';
 
-const UserList = ({ firebase }) => {
+class UserList extends Component {
+  _initFirebase = false;
 
-  const [_initFirebase, set_initFirebase] = useState(false);  
-  const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState([])
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
-    firebaseInit();
-    return firebase
-  }, [firebase])
- 
+    this.state = {
+      loading: false,
+      users: [],
+    };
+  }
 
-   const firebaseInit = () => {
-    if (firebase && !_initFirebase) {
-      set_initFirebase(true);
-      setLoading(true);
+  firebaseInit = () => {
+    if (this.props.firebase && !this._initFirebase) {
+      this._initFirebase = true;
+
+      this.setState({ loading: true });
 
       // this.props.firebase.users().on('value', snapshot => {
       //   const usersObject = snapshot.data();
@@ -133,7 +35,7 @@ const UserList = ({ firebase }) => {
       // });
 
       // TODO updated to firebase calls
-      firebase.users().get()
+      this.props.firebase.users().get()
         .then( snapshot => {
           console.log('snapshot in userslist: ', snapshot);
           // const usersObject = snapshot.data();
@@ -141,17 +43,34 @@ const UserList = ({ firebase }) => {
 
           const usersList = snapshot.docs.map(doc => doc.data());
 
-          setUsers(usersList);
-          setLoading(false);
+          // const usersList = Object.keys(usersObject).map(key => ({
+          //   ...usersObject[key],
+          //   uid: key,
+          // }));
+
+          this.setState({
+            users: usersList,
+            loading: false,
+          })
         })
     }
   };
 
+  componentDidMount() {
+    this.firebaseInit();
+  }
+
+  componentDidUpdate() {
+    this.firebaseInit();
+  }
 
   // TODO firbase equivalent for unmounting
   // componentWillUnmount() {
   //   this.props.firebase.users().off();
   // }
+
+  render() {
+    const { users, loading } = this.state;
 
     return (
       <div>
@@ -175,6 +94,8 @@ const UserList = ({ firebase }) => {
         </ul>
       </div>
     );
+  }
 }
+
 
 export default withFirebase(UserList);
