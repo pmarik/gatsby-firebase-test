5a13fc9ec618e57bcadfcede5624ddc55bc572dc
pmarik <pmarik@uwalumni.com>
2020-06-06 13:13:15 -0500

    header graphics and responsive styles for home page process section

--- src/components/Landing/ProcessSection.component.jsx
+++ src/components/Landing/ProcessSection.component.jsx
@@ -7,11 +7,13 @@ const ProcessSection = () => {
         <section  id="process" className="home-section process">
 				<div class="content-container">
                     <div className="process-text">
-                        <h2>
-                            <span className="process-bubble">Design</span> 
-                            <span className="process-bubble">Develop</span> 
-                            <span className="process-bubble">Deploy</span>
-                        </h2>
+                        <div className="process-header">
+                            <h2 className="process-bubble">Design</h2> 
+                            <span className="arrow"></span>
+                            <h2 className="process-bubble">Develop</h2> 
+                            <span className="arrow"></span>
+                            <h2 className="process-bubble">Deploy</h2>
+                        </div>
                         <p>Get a guided process from idea to reality. Full design ideation, custom development, and tailored deployment for your online needs. I create pixel perfect designs and engaging experiences to help tell your story in an impactful way. Solidify your brand and sell your services and products online now. </p>
                     </div>
                     <div className="process-image">
@@ -22,4 +24,4 @@ const ProcessSection = () => {
     )
 }
 
-export default ProcessSection;
+export default ProcessSection; 
